cd671c78ed2e296f2f1cf8e552d8266fc18ad5df
caio de oliveira bezerra <57061719+CaBezOf@users.noreply.github.com>
2021-05-13 21:11:06 -0300

    ver. alpha.0.16-final

--- store/models.py
+++ store/models.py
@@ -8,9 +8,11 @@ class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200)
+	
 
 	def __str__(self):
 		return self.name
+
 
 
 class Product(models.Model):
--- store/urls.py
+++ store/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        #Leave as empty string for base url
+	#Leave as empty string for base url
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
 	
+	path('login/', views.loginPage, name="login"),
+	path('register/', views.registerPage, name="register"),
 
-	path('update_item/', views.updateItem, name='update_item'),
-	path('process_order/', views.processOrder, name="process_order")
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+
 ]
